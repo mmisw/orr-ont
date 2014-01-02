8ef89626a4224951577a06bfb512602d328f3aa5
Carlos Rueda <carueda@mbari.org>
2014-01-01 16:50:02 -0800

    add some libs

--- project/build.scala
+++ project/build.scala
@@ -22,12 +22,16 @@ object OrrOntBuild extends Build {
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "org.scalatra"              %% "scalatra"             % ScalatraVersion,
+        "org.scalatra"              %% "scalatra-scalate"     % ScalatraVersion,
+        "org.scalatra"              %% "scalatra-specs2"      % ScalatraVersion % "test",
+        "com.typesafe"               % "config"               % "1.0.2",
+        "org.json4s"                %% "json4s-native"        % "3.2.4",
+        "com.typesafe"              %% "scalalogging-slf4j"   % "1.0.1",
+
+        "ch.qos.logback"             % "logback-classic"      % "1.0.6" % "runtime",
+        "org.eclipse.jetty"          % "jetty-webapp"         % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty.orbit"    % "javax.servlet"        % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
