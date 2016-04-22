0e1eb76dd3fe22409f5b29087f4fa5dc7324f96c
Carlos Rueda <carueda@gmail.com>
2016-04-22 13:23:26 -0700

    for failed login, report generic 401 (invalid credentials) instead of 404 (user not found)

--- src/main/scala/org/mmisw/orr/ont/app/FirebaseController.scala
+++ src/main/scala/org/mmisw/orr/ont/app/FirebaseController.scala
@@ -2,6 +2,9 @@ package org.mmisw.orr.ont.app
 
 import com.typesafe.scalalogging.{StrictLogging => Logging}
 import org.mmisw.orr.ont.Setup
+import org.mmisw.orr.ont.service.NoSuchUser
+
+import scala.util.{Failure, Success, Try}
 
 class FirebaseController(implicit setup: Setup) extends BaseController
       with Logging {
@@ -11,7 +14,12 @@ class FirebaseController(implicit setup: Setup) extends BaseController
     val map = body()
     val userName = require(map, "username")
     val password = require(map, "password")
-    val user = getUser(userName)
+
+    val user = Try(userService.getUser(userName)) match {
+      case Success(res)            => res
+      case Failure(exc: NoSuchUser) => error(401, "invalid credentials")
+      case Failure(exc)             => error500(exc)
+    }
 
     if (userAuth.checkPassword(password, user)) {
       val custom = Map(
